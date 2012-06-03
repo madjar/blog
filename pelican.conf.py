449ddd58fbd28ec22ae064f88660c558a1fe6a57
Georges Dubus <georges.dubus@compiletoi.net>
2012-06-03 18:18:18 +0200

    It works, let's start to blog.

---
+++ pelican.conf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Georges Dubus"
+SITENAME = u"Compile-toi toi même"
+SITESUBTITLE = u"(Georges Dubus)"
+SITEURL = '/'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+LOCALE = ('en_US.UTF-8', 'fr_FR.UTF8')
+
+THEME = 'stolenidea'
+
+MENUITEMS = (
+	('Archives', SITEURL + 'archives.html'),
+	('Tags', SITEURL + 'tags.html')
+)
+
+# Blogroll
+# LINKS = (
+#     ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+#     ('Python.org', 'http://python.org'),
+#     ('Jinja2', 'http://jinja.pocoo.org'),
+#     ('You can modify those links in your config file', '#')
+#          )
+
+# Social widget
+SOCIAL = (
+          ('Github', 'https://github.com/madjar'),
+          ('Twitter', 'http://twitter.com/georgesdubus'),
+          ('Google+', 'https://plus.google.com/u/0/104750974388692229541'),
+         )
+
+DEFAULT_PAGINATION = False
+
+
+PATH = ('src')
+FILES_TO_COPY = (('CNAME', 'CNAME'),)
+
+# TWITTER_USERNAME = 'georgesdubus'
+DISQUS_SITENAME = 'compiletoi'
+GOOGLE_ANALYTICS = 'UA-31800325-1'
---
+++ src/first_post.rst
@@ -0,0 +1,8 @@
+First post
+==========
+:date: 2012-06-03 17:20
+:tags: thats, awesome
+
+This is a first post.
+
+The name is temporary, as is the content.
